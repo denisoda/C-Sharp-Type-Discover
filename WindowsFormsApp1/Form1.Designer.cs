6185c4d268b31245cd593859d7ec91127f609a0b
Ilya Dzeraziak <ilya.by@bk.ru>
2017-06-07 21:01:28 +0300

    Add files via upload

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTypeName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lstMethods = new System.Windows.Forms.ListBox();
+            this.lstProperties = new System.Windows.Forms.ListBox();
+            this.lstConstructors = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(379, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(342, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Discover type information";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(29, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Type Name";
+            // 
+            // txtTypeName
+            // 
+            this.txtTypeName.Location = new System.Drawing.Point(134, 26);
+            this.txtTypeName.Name = "txtTypeName";
+            this.txtTypeName.Size = new System.Drawing.Size(228, 20);
+            this.txtTypeName.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Methods";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(256, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 18);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Properties";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(503, 51);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 18);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Constructors";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // lstMethods
+            // 
+            this.lstMethods.FormattingEnabled = true;
+            this.lstMethods.Location = new System.Drawing.Point(15, 73);
+            this.lstMethods.Name = "lstMethods";
+            this.lstMethods.Size = new System.Drawing.Size(238, 394);
+            this.lstMethods.TabIndex = 13;
+            // 
+            // lstProperties
+            // 
+            this.lstProperties.FormattingEnabled = true;
+            this.lstProperties.Location = new System.Drawing.Point(260, 73);
+            this.lstProperties.Name = "lstProperties";
+            this.lstProperties.Size = new System.Drawing.Size(240, 394);
+            this.lstProperties.TabIndex = 14;
+            // 
+            // lstConstructors
+            // 
+            this.lstConstructors.FormattingEnabled = true;
+            this.lstConstructors.Location = new System.Drawing.Point(507, 73);
+            this.lstConstructors.Name = "lstConstructors";
+            this.lstConstructors.Size = new System.Drawing.Size(214, 394);
+            this.lstConstructors.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(204)))), ((int)(((byte)(100)))));
+            this.ClientSize = new System.Drawing.Size(733, 477);
+            this.Controls.Add(this.lstConstructors);
+            this.Controls.Add(this.lstProperties);
+            this.Controls.Add(this.lstMethods);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtTypeName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Type Discover C#";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTypeName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox lstMethods;
+        private System.Windows.Forms.ListBox lstProperties;
+        private System.Windows.Forms.ListBox lstConstructors;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.SqlServer.Server;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        bool clic;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        
+
+            if (clic == true)
+            {
+                lstMethods.Items.Clear();
+                lstConstructors.Items.Clear();
+                lstProperties.Items.Clear();
+               
+                
+            }
+
+            #region MethodList
+
+            
+
+           
+            string TypeName = txtTypeName.Text;
+            Type T= Type.GetType(TypeName);
+            try
+            {
+
+            MethodInfo[] methods = T.GetMethods();
+
+            foreach (MethodInfo method  in methods)
+            {
+                lstMethods.Items.Add(method);
+
+            }
+            }
+            catch (Exception ex)
+            {
+               MessageBox.Show(ex.Message);
+            }
+
+            #endregion
+
+
+            #region PropertiesList
+
+            
+
+            try
+            {
+
+               PropertyInfo[] properties = T.GetProperties();
+
+                foreach (PropertyInfo prop in properties)
+                {
+                    lstProperties.Items.Add(prop);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            #endregion
+
+            #region ConstructsList
+
+            
+
+            try
+            {
+
+               ConstructorInfo[] constrc = T.GetConstructors();
+
+                foreach (ConstructorInfo constr in constrc)
+                {
+                    lstConstructors.Items.Add(constr);
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            #endregion
+
+            clic = true;
+
+        }
+    }
+}
